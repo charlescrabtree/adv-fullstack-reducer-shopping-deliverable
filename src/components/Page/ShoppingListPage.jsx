fb577b4874654afdf9327a50e32588ea7536a7d7
charlescrabtree <charles.crabtree.horn@gmail.com>
2023-01-10 17:51:11 -0800

    refactored shoppinglist form, added shoppinglistform to shoppinglistpage

--- src/components/Page/ShoppingListPage.jsx
+++ src/components/Page/ShoppingListPage.jsx
@@ -1,5 +1,9 @@
+import ShoppingListForm from '../Shopping/ShoppingListForm.jsx';
+
+
 export default function ShoppingListPage() {
   return <section>
     <h1>My Shopping List</h1>
+    <ShoppingListForm />
   </section>;
 }
--- src/components/Shopping/ShoppingListForm.jsx
+++ src/components/Shopping/ShoppingListForm.jsx
@@ -1,21 +1,15 @@
 export default function ShoppingListForm({
   body,
-  deleteItem,
-  handleDeleteItem, 
-  handleSubmitItem,
-  onEditItem,
+  onBodyChanged,
+  onSubmit
 }) {
-  return <form className="form" onSubmit={(e) => {
+  return <form onSubmit={(e) => {
     e.preventDefault();
-    handleSubmitItem(body);
+    onSubmit(body);
   }}>
-    <textarea value={body} onChange={(e) => onEditItem(e.target.value)}/>
-    <div className="styles">
-      <button type="submit">add item</button>
-      { deleteItem 
-        ? <button onClick={handleDeleteItem}>delete item</button>
-        : ''
-      }
-    </div>
+    <textarea value={body} onChange={(e) => {
+      onBodyChanged(e.target.value);
+    }}/>
+    <button type="submit">add item</button>
   </form>;
 }
